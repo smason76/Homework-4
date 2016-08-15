aa93daab9e739c160e54c6a339a8c10af45dcdf3
Stephanie Mason <nosamms@yahoo.com>
2016-08-15 00:50:17 -0700

    Finished homework

---
+++ Relaxr.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    //click handler for read more link
+//    $('.readmore').click(function () {
+//        $('#show-this-on-click').slideDown();
+//        //                alert('Hello');
+//        $('.readmore').hide();
+//        $('.readless').show();
+//    });
+//    
+//    //click handler for read less link
+//    $('.readless').click(function () {
+//        $('#show-this-on-click').slideUp();
+//        //                alert('Hello');
+//        $('.readmore').show();
+//        $('.readless').hide();
+//    });
+    
+    //click handler for learn more link
+    $('.learnmore').click(function () {
+        $('#learnmoretext').slideDown();
+        //                alert('Hello');
+        $('.learnmore').hide();
+    });
+    
+    
+  //setting up the clickhandlers for the read more/ read less function  
+     $('.readmore').click(function () {
+        toggleReadMore();
+     });
+    
+    $('.readless').click(function () {
+        toggleReadMore();
+     });
+    
+    
+    
+});
+//combined read more / read less link
+function toggleReadMore(){
+    $('#show-this-on-click').toggle();
+        //                alert('Hello');
+        $('.readmore').toggle();
+        $('.readless').toggle();
+    };
